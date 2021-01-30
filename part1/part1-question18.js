63f986cf806f884d0decec506a97ec752f414d59
Anoushka Dave <anoushkadave@MacBook-Air.lan>
2021-01-30 04:10:16 -0800

    part 1, ques 1-18 done

---
+++ part1/part1-question18.js
@@ -0,0 +1,15 @@
+let statistics = {
+    redCars: 21, 
+    blueCars: 45, 
+    greenCars: 12,
+    raceCars: 5, 
+    blackCars: 40, 
+    rareCars: 2
+  };
+  
+  for (property in statistics) {
+    // print if first of property letter is r or if value of property is odd
+    if ((property.substr(0, 1) == "r") || (statistics[property] % 2 != 0)) {
+      console.log(statistics[property]);
+    }
+  }
